b12c323cdb0fcf4c032dd43ba77b9e0b82701c0a
Qiwei Yang <yangqiwei97@gmail.com>
2019-05-13 21:37:01 +1000

    Use environment variables in our functions

--- create.js
+++ create.js
@@ -1,14 +1,14 @@
 import uuid from "uuid";
-import * as dynamoDbLib from "../libs/dynamodb-lib";
-import { success, failure } from "../libs/response-lib";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
   const params = {
-    TableName: process.env.tableName,
+    TableName: "notes",
     Item: {
-      userid: event.requestContext.identity.cognitoIdentityId,
-      noteid: uuid.v1(),
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: uuid.v1(),
       content: data.content,
       attachment: data.attachment,
       createdAt: Date.now()
@@ -19,7 +19,6 @@ export async function main(event, context) {
     await dynamoDbLib.call("put", params);
     return success(params.Item);
   } catch (e) {
-    console.log(e);
     return failure({ status: false });
   }
 }
--- get.js
+++ get.js
@@ -1,15 +1,15 @@
-import * as dynamoDbLib from "../libs/dynamodb-lib";
-import { success, failure } from "../libs/response-lib";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
   const params = {
-    TableName: process.env.tableName,
+    TableName: "notes",
     // 'Key' defines the partition key and sort key of the item to be retrieved
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
     Key: {
-      userid: event.requestContext.identity.cognitoIdentityId,
-      noteid: event.pathParameters.id
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
     }
   };
 
